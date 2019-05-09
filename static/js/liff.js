440562d38a554ea49ed2572f001a3f8f50691376
Koki Miura <memjoalty@yahoo.co.jp>
2019-05-10 02:02:48 +0900

    Changed uuid

--- static/js/liff.js
+++ static/js/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '980284c7-a254-4980-bd67-df91dc1748f8'; // LED, Button
+const USER_SERVICE_UUID         = 'c1473b48-c17f-4e34-acbe-b66feee850d0'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
