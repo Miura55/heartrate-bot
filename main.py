eb15d2ced4fedae0d793de88211443173f57a572
Koki Miura <memjoalty@yahoo.co.jp>
2019-05-14 01:44:35 +0900

    return false

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    username = db.Column(db.String(80), unique=False)
     heart_rate = db.Column(db.Integer)
     save_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
