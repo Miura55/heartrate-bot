71d3e51ead68663cd62564a8a1e9a5c3138f1255
Koki Miura <memjoalty@yahoo.co.jp>
2019-05-14 23:14:09 +0900

    Fix inyo

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def handle_message(event):
     if event.type == "message" and event.message.type == "text":
         if event.message.text == "今の心拍数":
             before_10s = datetime.now() - dt.timedelta(seconds=10)
-            userId = event.source.userId
+            userId = event["source"]["userId"]
             users = db.session.query(User).filter(User.save_date>=before_10s).filter(User.username==userId).all()
             message = ""
             for data in users:
