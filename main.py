9d04f9c41eeaa68cd03fa7c11ec405d2f3129ca4
Koki Miura <memjoalty@yahoo.co.jp>
2019-05-14 23:02:18 +0900

    Fix query

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def handle_message(event):
     if event.type == "message" and event.message.type == "text":
         if event.message.text == "今の心拍数":
             before_10s = datetime.now() - dt.timedelta(seconds=10)
-            users = db.session.query(User).filter(User.save_date>=before_10s).filter(User.username=event.source.userId).all()
+            users = db.session.query(User).filter(User.save_date>=before_10s).filter(User.username==event.source.userId).all()
             message = ""
             for data in users:
                 message += data.save_date.strftime('%Y-%m-%d %H:%M:%S') + " " + str(data.heart_rate) + "\n"
